c93291b635451f95c3ff2e34b7eb19ca0f7cf469
Sébastien Spangenberger <52532115+Sattaz@users.noreply.github.com>
2021-12-26 17:29:59 +0100

    Update OpenEVSE.class.php

--- core/class/OpenEVSE.class.php
+++ core/class/OpenEVSE.class.php
@@ -325,8 +325,8 @@ class OpenEVSE extends eqLogic {
 		$info->setType('info');
 		$info->setSubType('numeric');
 		$info->setTemplate('dashboard','line');
-		//$info->setConfiguration('minValue', $this->getConfiguration("AMin"));
-		//$info->setConfiguration('maxValue', $this->getConfiguration("AMax"));
+		$info->setConfiguration('minValue', 0); //$this->getConfiguration("AMin"));
+		$info->setConfiguration('maxValue', 32); //$this->getConfiguration("AMax"));
 		$info->setIsHistorized(1);
 		$info->setUnite('A');
 		$info->setOrder(2);
